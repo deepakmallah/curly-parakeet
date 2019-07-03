b358713987da7dd3239276ecace338ee91f39588
deepak <deepak.mallah@docsapp.in>
2019-07-03 19:36:10 +0530

    Linked list with insetAt function

--- linkedList.js
+++ linkedList.js
@@ -65,21 +65,20 @@ class LinkedList {
 
       var iteration = 0;
 
-      while(iteration < index) {
+      while(iteration <= index) {
+        if(iteration == index) {
+          node.next = current;
+          previous.next = node;
+
+          this.size++;
+        } else {
+          previous = current;
+          current = current.next;
+        }
+
         iteration++;
-
-        previous = current;
-        current = current.next;
       }
-
-      // previous = current.next
-      // current.next = node;
-
-      node.next = current;
-      previous.next = node;
     }
-
-    this.size++;
   }
 
   removeFrom(index) {
@@ -91,20 +90,52 @@ class LinkedList {
 
     if(index == 0) {
       this.head = current.next;
+      this.size--;
     } else {
       while(iteration < index) {
         iteration++;
 
-        previous = current
+        previous = current;
         current = current.next;
       }
 
       previous.next = current.next;
+      this.size--;
     }
 
-    this.size--;
-
     return current.element;
+  }
+
+  removeElement(element) {
+    var current = this.head;
+    var previous = null;
+    var iteration = 0;
+
+    while(iteration < this.size) {
+      if(current.element == element) {
+
+        previous.next = current.next;
+        current = current.next;
+
+        this.size--;
+      } else {
+        previous = current;
+        current = current.next;
+      }
+
+      iteration++
+    }
+  }
+
+  isEmpty() {
+    let status = !this.size;
+    console.log("Is Empty", status)
+
+    return status;
+  }
+
+  sizeOfList() {
+    console.log("size is", this.size)
   }
 
   PrintList(){
@@ -112,32 +143,39 @@ class LinkedList {
     var str = '';
 
     while(current) {
-      str += current.element + " ";
+      str += current.element + ", ";
 
       current = current.next;
     }
 
     console.log("List", str)
   }
-
-  // functions to be implemented
-  // removeElement(element)
-
-  // Helper Methods
-  // isEmpty
-  // size_Of_List
-  // PrintList
 }
 
 
 var LL = new LinkedList();
 
+LL.isEmpty();
+LL.sizeOfList();
 LL.add("a");
 LL.add("b");
 LL.add("c");
 
 LL.removeFrom(2);
+LL.PrintList();
 
 LL.add("d");
-
 LL.PrintList();
+LL.add("e");
+LL.add("f");
+LL.sizeOfList();
+LL.add("g");
+LL.add("e");
+LL.add("h");
+LL.removeElement('e');
+LL.add("i");
+
+LL.insertAt("z", 1);
+LL.PrintList();
+LL.sizeOfList();
+LL.isEmpty();
